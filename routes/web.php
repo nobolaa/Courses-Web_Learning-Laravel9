b8aea95d7b2300bac15591737bf31d0414320e2f
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-16 17:19:46 +0200

    Created some routes at routes/web.php with basic text lines on return.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    /*return view('welcome');*/
+    return "Bienvenido a la página principal";
+});
+
+Route::get('cursos', function(){
+    return "Bienvenido a la página cursos";
+});
+
+Route::get('cursos/create', function () {
+    return "En esta página podrás crear un curso";
+});
+
+/* Route::get('cursos/{curso}', function($curso){
+    return "Bienvenido al curso: $curso";
+});
+
+Route::get('cursos/{curso}/{categoria}', function($curso, $categoria){
+    return "Bienvenido al curso $curso, de la categoría $categoria";
+});
+*/
+
+Route::get('cursos/{curso}/{categoria?}', function($curso, $categoria = null){
+    if($categoria){
+        return "Bienvenido al curso $curso, de la categoría $categoria";
+    }
+    else{
+        return "Bienvenido al curso: $curso";
+    }
 });
