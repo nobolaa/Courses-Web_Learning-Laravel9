b7e092118a72d37c165bee855cac054e2012957e
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-16 20:28:05 +0200

    Added column category to table courses (on create migration). Added seeds to courses table at DatabaseSeeder.php file.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Curso;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $curso = new Curso();
+
+        $curso->name = "Laravel";
+        $curso->descripcion = "El mejor framework de Laravel";
+        $curso->categoria = "Desarrollo Web";
+
+        $curso->save();
+
+        $curso2 = new Curso();
+
+        $curso2->name = "Laravel2";
+        $curso2->descripcion = "El mejor framework de Laravel2";
+        $curso2->categoria = "Desarrollo Web2";
+
+        $curso2->save();
+
+        $curso3 = new Curso();
+
+        $curso3->name = "Laravel3";
+        $curso3->descripcion = "El mejor framework de Laravel3";
+        $curso3->categoria = "Desarrollo Web3";
+
+        $curso3->save();
     }
 }
