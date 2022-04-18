d8fa6dfd0b4a9fc49b1a54a4ba4246996bbaf5d5
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-18 20:41:31 +0200

    Added some features:     - List of courses from database to view courses index page.     - Information from one specific course at course show page.     - Modified routes file to get this information from database.

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curso;
 use Illuminate\Http\Request;
 
 class CursoController extends Controller
 {
     public function index(){
-        return view('cursos.index');
+        $cursos = Curso::paginate();
+        
+        return view('cursos.index', compact('cursos'));
     } 
 
     public function create(){
         return view('cursos.create');
     }
 
-    public function show($curso){
-
+    public function show($id){
+        $curso = Curso::find($id);
         return view('cursos.show', compact('curso')); //compact = ['curso' => $curso]
     }
 }
